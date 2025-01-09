7c3ca25f5c295ff6b7a8b27e828ef20ecc0cd8f9
kolech <hailejesus@gmail.com>
2025-01-08 21:45:23 -0500

    implement set data type as ordered list

--- ch2/unordered_list.rkt
+++ ch2/unordered_list.rkt
@@ -1,5 +1,5 @@
 #lang sicp
-(define (element-of-set? x  set)
+(define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
